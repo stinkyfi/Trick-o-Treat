6cfddf9b0984e1067ec3cafdde765be23209538f
Stinky Finance <83800142+stinkyfi@users.noreply.github.com>
2021-10-03 21:40:46 -0700

    Update trickOTreat.sol
    cleaning code up

--- Contract/trickOTreat.sol
+++ Contract/trickOTreat.sol
@@ -1,7 +1,7 @@
 /*******************************************
  * This contract is an Experiment, and should be used at your own risk.
  * 
- * Author: Stinky Fi 
+ * Author: Stinky Fi
  * Name:   Trick-O-Treat
  * Desc:   Knock on our door, and say the magic words, if you dare!
  *         Minters will receive a Trick or a Treat, determined by on-chain randomness.
@@ -17,19 +17,20 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract trickOTreat is ERC721Enumerable, ReentrancyGuard, Ownable {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
     
-    //Halloween Night 11:59:59 Contract Expires
+    // Hautning starts (Saturday, October 30, 2021 12:00:00 AM) GMT
     uint256 public startHaunt = 1635724799;
-    uint256 public endHaunt = 1635552000;
-    uint256 public lastMint     = 0;
-    uint256 public tokenId;
-    uint256 public price = 1000000000000000000; //1 FTM
-    
-    string   private image;
+    // Haunting ends (Sunday, October 31, 2021 11:59:59 PM) GMT
+    uint256 public endHaunt   = 1635552000;
+     // Mint Price 1 FTM
+    uint256 public price      = 1000000000000000000;
+
     // Trick Rarities
     string[] private tricks;
     string[] private rare_tricks;
@@ -38,13 +39,14 @@ contract trickOTreat is ERC721Enumerable, ReentrancyGuard, Ownable {
     string[] private treats;
     string[] private rare_treats;
     string[] private legendary_treats;
-
     
     address public beneficiary;
+    //Single mint Freebie
     mapping (address => bool) public freebie;
+    //Five Free mints
+    mapping (address => uint256) public winner;
     
-    constructor(address _beneficiary) ERC721("Test", "OOOO") {
-        console.log("Hello");
+    constructor(address _beneficiary) ERC721("Trick-O-Treat", "ToT") {
         beneficiary = _beneficiary;
     }
 
@@ -62,36 +64,44 @@ contract trickOTreat is ERC721Enumerable, ReentrancyGuard, Ownable {
 
     function freeKnock() public nonReentrant {
         bool _haunting = witchingHour();
-        require(_haunting, "This Contract is now longer Haunted");
+        require(_haunting, "This Contract is no longer Haunted");
         require(!freebie[msg.sender], "You have already used your free claim");
-        tokenId = lastMint + 1;
-        _safeMint(_msgSender(), tokenId);
+        _tokenIds.increment();
+        _safeMint(_msgSender(), _tokenIds.current());
         freebie[msg.sender] = true;
-        lastMint = tokenId;
+    }
+    
+    function contestWinner() public nonReentrant {
+        bool _haunting = witchingHour();
+        require(_haunting, "This Contract is no longer Haunted");
+        require(winner[msg.sender] == 0, "Your name is not on the list, rejected.");
+        for(uint256 i = 0; i <= winner[msg.sender]; i++)
+        {
+            _tokenIds.increment();
+            _safeMint(_msgSender(), _tokenIds.current());
+        }
+        delete winner[msg.sender];
     }
     
     function claim() public payable nonReentrant {
         bool _haunting = witchingHour();
-        require(_haunting, "This Contract is now longer Haunted");
+        require(_haunting, "This Contract is no longer Haunted");
         require(price == msg.value, "Incorrect payment amount");
-        tokenId = lastMint + 1;
-        _safeMint(_msgSender(), tokenId);
+        _tokenIds.increment();
+        _safeMint(_msgSender(), _tokenIds.current());
         payable(beneficiary).transfer(msg.value);
-        lastMint = tokenId;
     }
 
     function claimHandfull(uint256 _mintAmount) public payable {
         bool _haunting = witchingHour();
-        require(_haunting, "This Contract is now longer Haunted");
+        require(_haunting, "This Contract is no longer Haunted");
         uint256 _bundle = _mintAmount * price;
-        console.log(_bundle);
         require(_bundle == msg.value, "Incorrect payment amount");
-        console.log("we in here");
         
         for(uint256 i = 0; i < _mintAmount; i++)
         {
-            tokenId += 1;
-            _safeMint(_msgSender(), tokenId);
+            _tokenIds.increment();
+            _safeMint(_msgSender(), _tokenIds.current());
         }
         
         payable(beneficiary).transfer(msg.value);
@@ -106,25 +116,31 @@ contract trickOTreat is ERC721Enumerable, ReentrancyGuard, Ownable {
         return uint256(keccak256(abiEncoded));
     }
     
-    function bowlGrab(uint256 _tokenId) public view returns (string memory) {
-        uint256 rand = random(_tokenId, "trickOtreat");
-        // rand = rand % 2;
-        
+    function bowlGrab(uint256 _tokenId) internal view returns (string memory) {
+        uint256 rand = random(_tokenId, "Trick-O-Treat");
+
         if(rand % 6 == 0)
         {
             //Trick
-            if(rand % 21 == 0) // Rare Trick
+            if(rand % 16 == 0) // Rare 
+            {   
+                if(rand % 26 == 0) //legendary Trick
+                    return legendary_tricks[rand % legendary_tricks.length];
                 return rare_tricks[rand % rare_tricks.length];
+            }
             return tricks[rand % tricks.length];
         }
         else
         {
             //Treat
-            if(rand % 11 == 0) //Rare Treat
-                return rare_treats[rand % rare_treats.length]; 
+            if(rand % 16 == 0) //Rare Treat
+            {
+                if(rand % 10 == 0)
+                    return legendary_treats[rand * legendary_treats.length];
+                return rare_treats[rand % rare_treats.length];    
+            }
             return treats[rand % treats.length];
         }
-
     }
     
     function toString(uint256 value) internal pure returns (string memory) {
@@ -153,10 +169,19 @@ contract trickOTreat is ERC721Enumerable, ReentrancyGuard, Ownable {
         return string(buffer);
     }
     
+    // All of the minting functions, will only operate during Halloween weekend 2021
     function witchingHour() public view returns (bool) {
         if(block.timestamp > startHaunt && block.timestamp < endHaunt)
             {return true;}
         else
             {return false;}
     }
+    
+    // Contest Winners and Partnerships
+    function assignWinners(address[] memory _winners, uint256 winnings) public onlyOwner {
+        for (uint i=0; i < _winners.length; i++) {
+            winner[_winners[i]] = winnings;
+        }
+    }
+    
 }
